76ade267c4d7015677258071e51be9385385c96a
sakaki <sakaki@samubuc.co.jp>
2022-03-27 15:21:19 +0900

    tutorial3 use pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,12 +36,46 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
+      _setCounterValue();
       if (_counter % 2 == 0) {
         _type = "偶数";
       } else {
         _type = "奇数";
       }
     });
+  }
+
+  void _getCounterValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = prefs.getInt('counter') ?? 0;
+    });
+  }
+
+  void _setCounterValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter', _counter);
+  }
+
+  void _removeCounterValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = 0;
+      prefs.remove('counter');
+    });
+  }
+
+  void _redirectGoogle() async {
+    String url = Uri.encodeFull("https://www.google.co.jp");
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCounterValue();
   }
 
   @override
@@ -58,6 +94,14 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headline4,
             ),
             Text(_type, style: const TextStyle(fontSize: 20, color: Colors.red)),
+            ElevatedButton(
+              child: const Text("Reset"),
+              onPressed: _removeCounterValue,
+            ),
+            IconButton(
+              icon: const Icon(Icons.open_in_browser),
+              onPressed: () => {_redirectGoogle()},
+            ),
           ],
         ),
       ),
